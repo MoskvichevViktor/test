d64018ee83f602a651ad827c1f59366f776777eb
VMoskvichev <VMoskvichev@cbgr.ru>
2023-12-26 14:25:41 +0300

    test 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("а теперь вот так");
+
     }
 }
