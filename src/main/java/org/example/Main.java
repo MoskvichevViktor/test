1e0133ecb499769ada4edc1c71dc26fbc9c98b65
Moskvichev Viktor <beldetal@yandex.ru>
2024-01-03 18:23:44 +0300

    добавим ещё один коммит

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("а теперь вот так");
-
+        System.out.println("добавим ещё один коммит");
     }
 }
